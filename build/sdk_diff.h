ba880f1cc65b443ce247fa6c603ebe774721914e
Ruslan <uncleruc2075@gmail.com>
2025-12-19 02:23:58 +0300

    Add sdk_diff.h for modders tools

---
+++ build/sdk_diff.h
@@ -0,0 +1,14 @@
+// SDK DIFF for modders tools
+
+const char *sdk_diff[] = {
+NULL
+};
+
+const int sdk_diff_format = 0;
+const int sdk_diff_size = sizeof(sdk_diff)/sizeof(sdk_diff[0]);
+
+const char **sdk_shared_textarray = sdk_diff;
+const int *sdk_shared_size = &sdk_diff_size;
+
+// Generated on Mon Sep  8 02:14:06 MSK 2025
+
